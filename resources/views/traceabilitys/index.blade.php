285c09aa6c6aa6d80aa84d6b2fc148e285c65ce0
SaiHanAung <hctskhnhc@gmail.com>
2022-05-26 10:18:48 +0700

    edit traceability

--- resources/views/traceabilitys/index.blade.php
+++ resources/views/traceabilitys/index.blade.php
@@ -15,9 +15,8 @@
             @endforeach
 
             <?php
-            $date_of_use = thaidate('d - m - Y', strtotime($get_data_trac_use_fact->date_of_use));
-            $harvest_date = thaidate('d - m - Y', strtotime($get_data_trac_harv->harvest_date));
-            // dd($date_of_use);
+            $date_of_use = thaidate('d - m - Y', strtotime($get_data_trac_use_fact));
+            $harvest_date = thaidate('d - m - Y', strtotime($get_data_trac_harv));
             ?>
             <div class="row">
                 <label>เริ่มปลูกตั้งแต่วันที่ : <span>{{ $date_of_use }}</span></label>
