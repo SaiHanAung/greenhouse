64ae3682f38269c5b48065419f0d94393ed95b12
SaiHanAung <hctskhnhc@gmail.com>
2022-05-26 22:04:30 +0700

    eidt traceability

--- resources/views/traceabilitys/index.blade.php
+++ resources/views/traceabilitys/index.blade.php
@@ -7,7 +7,7 @@
         <div class="blur-box">
             @foreach($get_data_plot as $key_plot => $value_plot)
             <center>
-                <img src="{{ asset('plot_images/'.$value_plot->file_path) }}" alt="Image" width="25%">
+                <img src="{{ asset('plot_images/'.$value_plot->file_path) }}" alt="Image" width="50%">
             </center>
             <div class="row">
                 <label>แปลง : <span>{{ $value_plot->name }}</span></label>
